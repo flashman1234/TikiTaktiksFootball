46f6e0cfe7dbf75081eee1c6bce6553e8baa5d9b
meathaus <flashman1234@gmail.com>
2012-06-21 19:25:10 +0200

    Initial Commit

---
+++ TikiTaktiksFootball/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TikiTaktiksFootball
+//
+//  Created by Michal Thompson on 6/21/12.
+//  Copyright NA 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ TikiTaktiksFootball/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TikiTaktiksFootball
+//
+//  Created by Michal Thompson on 6/21/12.
+//  Copyright NA 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
